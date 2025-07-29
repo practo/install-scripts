36ed5ef510c900e55e92aa894a3c285fce29cdf0
Mayank Dharwa <mayankdharwa@gmail.com>
2025-07-29 21:54:52 +0530

    Create installer-0.1.0-rc4.sh

---
+++ wasabi/installer-0.1.0-rc4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+VERSION="v0.1.0-rc4"
+
+DIRNAME="wasabi_"$VERSION
+
+# Clone the repository from GitHub
+git clone --depth 1 -b $VERSION git@github.com:practo/wasabi.git $DIRNAME
+
+# Change directory into the cloned repository
+cd $DIRNAME
+
+# Run the setup.sh script
+sh setup.sh
+
+# Delete the cloned repository
+cd ..
+
+echo "Removing temporary directory $DIRNAME"
+rm -rf $DIRNAME
+
+echo "Wasabi Installed Successfully"
